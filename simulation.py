f8a72366aa77219f110887c63865526669ccd35a
thomas5mc <thomasher611@gmail.com>
2020-01-22 18:57:00 -0800

    updated functionality of code. Still needs it implement some code for checking angles.

--- simulation.py
+++ simulation.py
@@ -2,67 +2,162 @@ import tkinter
 import math
 import time as t1
 import numpy as np
+import matplotlib.pyplot as plt
 
+l1 = 2 #2cm from the center of rotation to the laser-y (change with measurements!)
+l2 = 2 #2cm from the center of rotation to the laser-x (change with measurements!)
+XMAX = 50 #50cm to signify the largest x value and the dimension of box
+YMAX = 50 #50cm to signify the largest x value and the dimension of box
 #initializing some global variables
 running = False                                 #running is false when no button is pressed, 
                                                 #   true when a button is pressed
 u = [[0.0],                                       #u vector holds our input values(left velocity, right velocity)
      [0.0]]
-t = 0.6                                         #t is a variable that holds a simulated time that considers
+t = 6                                           #t is a variable that holds a simulated time that considers
                                                 #   the time that a velocity is acting on a wheel
-theta = 0                                       #theta holds value of the angle between body's y axis and fixed axis
-dist_between_wheels = .010 # 1cm, random value, will need to update later
-X = [[0.0],[0.0],[0.0],[0.0],[0.0],[0.0]]     #X is a column vector [xb,yb,theta,x',y',theta']
+dist_between_wheels = .010                      # 1cm, random value, will need to update later
+X = [[0.0],[0.0],[0.0],[0.0],[0.0],[0.0]]       #X is a column vector [xb,yb,theta,x',y',theta']
+init_log_size = 500                             #initial size of the logging arrays
+xlog = [None]*init_log_size                     #create two arrays to store the values for state column vector 
+                                                #for logging and graphing purposes
+ylog = [None]*init_log_size                                       
+index = 0; #index for logging
 
+#initializes the starting point of the robot where (0,0) is the bottom left corner
 def initialize():
-    global theta
     global X
     #gather initial inputs to realize where the car is
     xb = float(input('Enter initial x position: '))
     yb = float(input('Enter initial y position: '))
     theta = float(input('Enter initial angle: '))
     X = [[xb],[yb],[theta],[0.0],[0.0],[0.0]]
-   
+    for ele in X:
+        print('eleX: ', ele)
+
 initialize()
 
+
+#graphing setup             adapted from https://pythonspot.com/matplotlib-update-plot/
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line, = ax.plot(xlog,ylog, marker=(3,0,X[2][0]))
+plt.xlabel('x(cm)')
+plt.ylabel('y(cm)')
+plt.title('State Estimation')
+plt.ylim(0,YMAX)                          #set the dimensions to be 0 to 50 cm
+plt.xlim(0,XMAX)
+
+
 def state_update():
-    global t
-    global theta
-    global X
-    global u
+    global X, u
+    global xlog, ylog, index
+    X[2][0] = math.radians(X[2][0])
+    X[5][0] = math.radians(X[5][0])
     A = [[1,0,0,t,0,0],                     #A is the state transition matrix that updates the x, y, and
          [0,1,0,0,t,0],                      #theta positioning by adding the previous velocoties multiplied my time
          [0,0,1,0,0,t],
          [0,0,0,0,0,0],
          [0,0,0,0,0,0],
          [0,0,0,0,0,0]]
-    B = [[0,0],
+    B = [[0,0],                             #B is a state transition matrix that takes our input and converts into x,y,and angular velocities
          [0,0],
          [0,0],
-         [0.5*math.cos(theta),0.5*math.cos(theta)],
-         [0.5*math.sin(theta),0.5*math.sin(theta)],
+         [0.5*math.cos(X[2][0]),0.5*math.cos(X[2][0])],
+         [0.5*math.sin(X[2][0]),0.5*math.sin(X[2][0])],
          [-1 * dist_between_wheels,dist_between_wheels]]
-    Bu = np.reshape(np.matmul(B,u),(6,1))
-    Ax = np.matmul(A,X)
-    X = np.add(np.matmul(A,X),np.reshape(np.matmul(B,u),(6,1)))
-    #TODO: fix the matrix multiplacation and add on the output model
-    for ele in Bu:
-        print('eleBu: ', ele)
-    for ele in Ax:
-        print('eleAx: ', ele)
+    Bu = np.reshape(np.matmul(B,u),(6,1))                                   #compute the value of B*u for state update
+    Ax = np.matmul(A,X)                                                     #compute value of A*x for state update
+    X = np.add(Ax,Bu)
+    X[5][0] = math.degrees(X[5][0])                                             #convert angular velocity for easier reading
+    X[2][0] = math.degrees(X[2][0])
+    #TODO: add on the output model
+    #for ele in Bu:
+    #    print('eleBu: ', ele)
+    #for ele in Ax:
+    #    print('eleAx: ', ele)
     for ele in X:
         print('eleX: ', ele)
+    #logging and graphing 
+    xlog[index] = X[0][0]
+    ylog[index] = X[1][0]
+    line.set_marker(marker=(3,0,X[2][0]+10))
+    line.set_xdata(xlog)
+    line.set_ydata(ylog)
+    fig.canvas.draw()
+    index +=1
+
+def check_angle():
+    global X
+    if X[2][0] < 90 and X[2][0] > 0:
+        if math.atan2((YMAX-X[1][0])/(XMAX-X[0][0])) < X[2][0]:
+            USEYMAX1 = True
+        else:
+            USEXMAX1 = True
+        if math.atan2((XMAX-X[1][0])/(X[1][0])) < (90-X[2][0]):
+            USEYMIN2 = True
+        else:
+            USEXMAX2 = True
+    elif X[2][0] < 180 and X[2][0] > 90:
+        if math.atan2((YMAX-X[1][0])/(X[0][0])) < (180 - X[2][0]):
+            USEYMAX1 = True
+        else:
+            USEXMIN1 = True
+        if math.atan2((YMAX-X[1][0])/(XMAX-X[0][0])) < (X[2][0]-90):
+            USEYMAX2 = True
+        else:
+            USEXMAX2 = True
+        
+    elif X[2][0] < 270 and X[2][0] > 180:
+        if math.atan2((X[1][0])/(X[0][0])) < (X[2][0]-180):
+            USEYMIN1 = True
+        else:
+            USEXMIN1 = True
+        if math.atan2((YMAX-X[1][0])/(X[0][0])) < (270 - X[2][0]):
+            USEYMAX2 = True
+        else:
+            USEXMIN2 = True
+    elif X[2][0] < 360 and X[2][0] > 270:
+        if math.atan2((XMAX-X[1][0])/(X[1][0])) < (360-X[2][0]):
+            USEYMIN1 = True
+        else:
+            USEXMAX1 = True
+        if math.atan2((X[1][0])/(X[0][0])) < (X[2][0]-270):
+            USEYMIN2 = True
+        else:
+            USEXMIN2 = True
+
+##output functions
+def output():
+    global X
+    check_angle()
+    if USEXMIN1:
+        D1 = (XMAX - X[0][0])/math.cos(math.radians(X[2][0])) -l1 #figure this out
+        D2 = (YMAX - X[1][0])/math.sin(math.radians(X[2][0])) -l2 #figure this out
+    #if X[2][0] < 180 and X[2][0] > 90:
+    #    D1 = 1 #figure this out
+    #    D2 = 1 #figure this out
+    #if X[2][0] < 270 and X[2][0] > 180:
+    #    D1 = 1 #figure this out
+    #    D2 = 1 #figure this out
+    #if X[2][0] < 360 and X[2][0] > 270:
+    #    D1 = 1 #figure this out
+    #    D2 = 1 #figure this out
+
+
 #driving imitation implementation
+#left and write inputs are velocities of each wheel
+#drive function will update 
 def drive(left, right):
     global u                                    #updates velocity of wheels
-    global running                              #indicates that car is running
-    running = True
+    #global running                              #indicates that car is running
+    #running = True
     u[0] = left
     u[1] = right
     state_update()
 
 def forward(event):
-    drive(1.0,1.0)                              #.01 meters per second speed
+    drive(1.0,1.0)                              #.01 meters per second speed, will need to update
     print('forward')
 def left(event):
     drive(-1.0,1.0) 
@@ -80,8 +175,8 @@ def stop(event):
 #function to handle when a button is released
 def button_release(event):
     global t
-    global running
-    running = False
+    #global running
+    #running = False
     print('hello')
 
 #creating a gui for controlling the car using tkinter
@@ -103,8 +198,6 @@ bw.bind('<ButtonRelease-1>', button_release)
 sp = tkinter.Button (window,text='stop')
 sp.bind('<ButtonPress-1>', stop)
 sp.bind('<ButtonRelease-1>', button_release)
-
-
 fd.grid(column=5,row=0)
 rt.grid(column=6,row=1)
 lt.grid(column=4,row=1)
